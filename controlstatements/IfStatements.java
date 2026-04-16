2edd639c22065728a38e37e7255bd12f1bdc9829
Vinod Pimpale <vinodpimpale18@gmail.com>
2026-04-16 23:19:14 +0530

    New thread is created and value is changed in old theread

--- controlstatements/IfStatements.java
+++ controlstatements/IfStatements.java
@@ -4,10 +4,10 @@ package com.controlstatements;
 public class IfStatements {
 	public static void main(String[] args) {
 		int a = 10;
-		if (a > 50) { // Note: if statement is true then if block is executed
+		if (a > 100) { // Note: if statement is true then if block is executed
 		}
 		System.out.println("A is greater");
-		if (a > 200) { // Note: condition is false so jvm will not enter into if block
+		if (a > 20) { // Note: condition is false so jvm will not enter into if block
 			System.out.println("A is smaller");
 		}
 	}
