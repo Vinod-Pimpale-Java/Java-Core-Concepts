e107f01381033c977b055fa98bdd75959fe3863e
Vinod Pimpale <vinodpimpale18@gmail.com>
2026-04-16 12:27:15 +0530

    New Values are added into array

--- Array/ForEachLoop.java
+++ Array/ForEachLoop.java
@@ -2,7 +2,7 @@ package com.Array;
 //Array Using ForEach Loop
 public class ForEachLoop {
 	public static void main(String[] args) {
-		 int arr[]= {11,22,33,44,55,66};
+		 int arr[]= {11,22,33,44,55,66,55,78,98};
 		 
 		 for(int Value:arr) {
 		 System.out.print(" "+Value+ " ");
